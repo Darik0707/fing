94253afd7e5c703ee95f4946f607938d6e8af551
Darik0707 <85736120+Darik0707@users.noreply.github.com>
2021-06-21 10:17:21 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -192,7 +192,10 @@ const video = document.querySelector("#pose-video");
     if (/iPhone|iPad|iPod/i.test(navigator.userAgent)) {
      document.getElementsByTagName('meta')[1].setAttribute( 'content', 'width=device-width,initial-scale=1');
       if(ratio == 1.78){
-         canvas.style.top = '15%';
+         canvas.style.top = '-20%';
+         canvas.style.left = '-50%';
+         
+        
       } else if ( ratio == 2){
         canvas.style.top = '21%';
       } else if (ratio == 2.11) {
